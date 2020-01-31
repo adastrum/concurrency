33980bbc647f17afb39d086f7201a1f893f7ef32
Dmitry Trofimov <trofimovde@outlook.com>
2020-01-31 21:30:24 +0100

    Add ConcurrentDictionary tests

--- Concurrency/Concurrency.Collections.Tests/ConcurrentDictionaryTests.cs
+++ Concurrency/Concurrency.Collections.Tests/ConcurrentDictionaryTests.cs
@@ -1,13 +1,59 @@
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Concurrency.Collections.Tests
 {
     public class ConcurrentDictionaryTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ITestOutputHelper _output;
 
+        public ConcurrentDictionaryTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task Should_iterate_while_adding()
+        {
+            var sut = new ConcurrentDictionary<int, int>();
+
+            var addDelay = 10;
+            var iterateDelay = 11;
+
+            var numberOfItems = 10;
+
+            var addTask = Task.Run(async () =>
+            {
+                for (var i = 0; i < numberOfItems; i++)
+                {
+                    sut.TryAdd(i, i);
+
+                    await Task.Delay(addDelay);
+                }
+            });
+
+            var actualNumberOfItems = 0;
+
+            var iterateTask = Task.Run(async () =>
+            {
+                await Task.Delay(addDelay);
+
+                foreach (var kvp in sut)
+                {
+                    //_output.WriteLine($"[{kvp.Key}] = {kvp.Value}");
+
+                    Interlocked.Increment(ref actualNumberOfItems);
+
+                    await Task.Delay(iterateDelay);
+                }
+            });
+
+            await Task.WhenAll(addTask, iterateTask);
+
+            Assert.Equal(numberOfItems, actualNumberOfItems);
         }
     }
 }
---
+++ Concurrency/Concurrency.Collections/ConcurrentDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+
+namespace Concurrency.Collections
+{
+    public class BestPractices
+    {
+        /// <summary>
+        /// Avoid unless necessary
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="concurrentDictionary"></param>
+        public void AcquireAllLocks<TKey, TValue>(ConcurrentDictionary<TKey, TValue> concurrentDictionary)
+        {
+            var count = concurrentDictionary.Count;
+            var isEmpty = concurrentDictionary.IsEmpty;
+            var keys = concurrentDictionary.Keys;
+            var values = concurrentDictionary.Values;
+
+            concurrentDictionary.Clear();
+            var array = concurrentDictionary.ToArray();
+        }
+
+        /// <summary>
+        /// Preferred way
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="concurrentDictionary"></param>
+        public void LockFreeEnumeration<TKey, TValue>(ConcurrentDictionary<TKey, TValue> concurrentDictionary)
+        {
+            var count = concurrentDictionary.Skip(0).Count();
+            var keys = concurrentDictionary.Select(x => x.Key);
+            var values = concurrentDictionary.Select(x => x.Value);
+        }
+    }
+}
